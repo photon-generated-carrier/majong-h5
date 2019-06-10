6f9abb6667a7d79b61f566baa38374419d7e69b5
photonCarry <yy251745080@qq.com>
2019-06-11 00:38:40 +0800

    change svr port to 80

--- express_demo.js
+++ express_demo.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 	res.sendFile( __dirname + "/" + "index.html" );
 })
 
-var server = app.listen(8080, function() {
+var server = app.listen(80, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("http://%s:%s", host, port)
